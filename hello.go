7d1772f95d661e0565446a9d00a93fd194fdb349
Hugo Slabbert <hugo@slabnet.com>
2021-03-16 03:15:13 +0000

    add remote module

--- hello.go
+++ hello.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"example.com/user/hello/morestrings"
+	"fmt"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
-	fmt.Println("Hello world.")
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
